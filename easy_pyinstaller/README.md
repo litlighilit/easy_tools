858ecf85ee96f22ffe11f2fb971d8b3700237928
litlighilit <litlighilit@foxmail.com>
2023-07-29 17:18:38 +0800

    Delete easy_pyinstaller directory
    To add a new one later

--- easy_pyinstaller/README.md
+++
@@ -1,2 +0,0 @@
-<h1>easy_installer</h1>
-a GUI tool that bail out ones who are fed up with such a terrible exprience when inputing in the cmd
--- easy_pyinstaller/_data.py
+++
@@ -1,7 +0,0 @@
-import os
-from sys import executable
-NAME="pyinstaller.exe"
-SPLIT=b"#!"
-ERROR=f"{NAME}'s struction to express python's path is changed somehow"
-SCRIPTS_PATH=os.path.join(os.path.dirname(executable),'Scripts')
-PYINST_PATH=os.path.join(SCRIPTS_PATH,NAME)
